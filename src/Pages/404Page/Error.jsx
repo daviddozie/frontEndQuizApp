302864afe28c64abc0ae1a754a08c4f919f85595
daviddozie <davidmgbede75@gmail.com>
2024-02-28 08:55:55 -0800

    update

--- src/Pages/404Page/Error.jsx
+++ src/Pages/404Page/Error.jsx
@@ -10,9 +10,9 @@ function Error() {
 
   return (
     <div data-theme={isDark ? "dark" : "light"}>
-      <div className='h-[100vh] w-full'>
+      <div className='min-h-[100vh] w-full'>
         <div className='flex justify-center items-center'>
-          <h1 className={`text-[${isDark}] font-[900] text-[8rem] mt-[150px]`}>404-Error</h1>
+          <h1 className={`text-[${isDark}] font-[900] md:text-[8rem] text-[2rem] mt-[10rem]`}>404-Error</h1>
         </div>
         <h3 className='text-[#048970] font-[600] text-[1rem] text-center'>Opps! Page Not Found</h3>
         <p className={`text-center text-[${isDark}] py-6`}>Click the button below 👇 to go back to meun</p>
